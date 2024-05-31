cd0a3390808c8950d110c9cd9f6827ce115f4f8c
Vladislav-Lazarev <168584793+Vladislav-Lazarev@users.noreply.github.com>
2024-05-31 21:19:41 +0400

    Initial commit

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -9,7 +9,7 @@ import java.io.*;
 import static java.lang.Thread.sleep;
 
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         String fileName = args[0];
         try {
             Connection connection = new ConnectionImpl();
